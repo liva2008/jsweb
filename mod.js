efa00240712f578dcb752d28c490b39b4cd18713
liva2008 <liva2008@qq.com>
2022-12-09 20:44:28 +0800

    v1.0

--- mod.js
+++ mod.js
@@ -1,8 +1,8 @@
-export * from './jsweb/application.js'; //应用
-export * from './jsweb/context.js';    //上下文 
-export * from './jsweb/middleware.js'; //中间件
-export * from './jsweb/router.js';     //路由器
-export * from './jsweb/cors.js';       //跨域
-export * from './jsweb/jwt.ts';        //JSON Web Token
-export * from './jsweb/mongodb.ts';    //mongodb
-export * from './jsweb/captcha.ts';    //验证码
+export * from './lib/application.js'; //应用
+export * from './lib/context.js';    //上下文 
+export * from './lib/middleware.js'; //中间件
+export * from './lib/router.js';     //路由器
+export * from './lib/cors.js';       //跨域
+export * from './lib/jwt.ts';        //JSON Web Token
+export * from './lib/mongodb.ts';    //mongodb
+export * from './lib/captcha.ts';    //验证码
