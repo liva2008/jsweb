6542b7e659e53a5540acb9b2d83daad62ed45ccc
liva2008 <liva2008@qq.com>
2022-12-13 20:59:23 +0800

    V1.0.7 增加mongodb连接池

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,10 +4,9 @@ import {
   GridFSBucket, 
   ObjectId
 } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+import { createPool } from "https://deno.land/x/generic_pool/mod.ts";
 
 export { GridFSBucket, ObjectId };
-
-const client = new MongoClient();
 
 /*
 1.Connecting to a Local Database:
@@ -39,6 +38,39 @@ await client.connect(
 */
 
 //await client.connect("mongodb://test:123456@127.0.0.1:27017/test");
-await client.connect("mongodb://127.0.0.1:27017");
+
+/**
+ * Step 1 - Create pool using a factory object
+ */
+ const factory = {
+  async create() {
+    const client = new MongoClient();
+    try {
+      await client.connect("mongodb://127.0.0.1:27017");
+    } catch (err) {
+      console.error(err);
+    }
+    return client;
+  },
+  async destroy(client) {
+    await client.close();
+  },
+};
+
+const opts = {
+  max: 10, // maximum size of the pool
+  min: 2 // minimum size of the pool
+};
+
+const pool = createPool(factory, opts);
+
+/**
+ * Step 2 - Use pool in your code to acquire/release resources
+ */
+
+// acquire connection - Promise is resolved
+// once a resource becomes available
+const client = await pool.acquire();
+//console.log(pool);
 
 export const db = client.database("jsweb");
